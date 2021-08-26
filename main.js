692ce65279881d80912db2e67183e526aa9e7016
nicolas-00 <ni_ha@bluewin.ch>
2021-08-26 16:13:54 +0200

    for loop

--- main.js
+++ main.js
@@ -4,25 +4,7 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-for (x = 0; x < 5; x++) {
-  const newImage = document.createElement('img');
-  newImage.setAttribute('src', 'images/pic' + x + '.jpg');
-  thumbBar.appendChild(newImage);
-  newImage.onclick = function (e) {
-    displayedImage.src = e.target.src;
-  }
-}
-
-
-btn.onclick = function(){
-  const btnClass = btn.getAttribute('class');
-if (btnClass = "dark") {
-  btn.setAttribute('light');
-  btn.textContent = "Lighten";
-  overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
-} else {
-  btn.setAttribute('dark');
-  btn.textContent = "Darken";
-  overlay.style.backgroundColor = "rgba(0,0,0,0)";
-}
+for(let i = 0; i < 5; i++)
+{ 
+  
 }
